21e2cb44cbc6e4d8742d821df05babdf32be1848
Jacob-109 <65383310+Jacob-109@users.noreply.github.com>
2023-02-03 21:25:27 +0800

    Add unit test for expect function

--- Team00/Code00/src/unit_testing/src/TestTokenizer.cpp
+++ Team00/Code00/src/unit_testing/src/TestTokenizer.cpp
@@ -1,34 +1,40 @@
 #include "Tokenizer.h"
 #include "Tokenizer.cpp"
 #include "catch.hpp"
-using namespace std;
+#include <regex>
 
-void match(bool b) {
-    REQUIRE(b);
-}
+using namespace std;
 
 TEST_CASE("TokenizeTest") {
     std::string testStatement = "Hello world testing";
+
     const char* args[] = { "Hello", "world", "testing"};
     std::vector<std::string> expected(args, std::end(args));
-    std::vector<std::string> output = tokenize(testStatement);
 
-    match(output == expected);
+    REQUIRE(tokenize(testStatement) == expected);
 }
 
 TEST_CASE("NullStringTest") {
     std::string testStatement = "";
     vector<string> expected;
-    std::vector<std::string> output = tokenize(testStatement);
-
-    match(output == expected);
+    REQUIRE(tokenize(testStatement) == expected);
 }
 
 TEST_CASE("EmptySpaceBeginningAndEndOfStringTest") {
     std::string testStatement = " Hello ";
+
     const char* args[] = {"Hello"};
     std::vector<std::string> expected(args, std::end(args));
-    std::vector<std::string> output = tokenize(testStatement);
 
-    match(output == expected);
+    REQUIRE(tokenize(testStatement) == expected);
+}
+
+TEST_CASE("ExpectFunctionTest") {
+    std::string testStatement = " Hello World";
+    REQUIRE(expect(testStatement,"Hello") == "Hello");
+}
+
+TEST_CASE("ExpectFunctionFailTest") {
+    std::string testStatement = " Hello World";
+    REQUIRE_THROWS_AS(expect(testStatement,"World"),std::invalid_argument);
 }
