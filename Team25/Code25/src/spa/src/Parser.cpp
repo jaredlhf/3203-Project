46027a1dc87713e4121be99c498313f07a57082e
Jacob-109 <65383310+Jacob-109@users.noreply.github.com>
2023-02-03 23:31:55 +0800

    Add isValidVariableName, isNumber, parseAssign and parseStatement to Parser class

--- Team25/Code25/src/spa/src/Parser.cpp
+++ Team25/Code25/src/spa/src/Parser.cpp
@@ -2,13 +2,67 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <regex>
 
 using namespace std;
 
-#include "PKB.h"
-#include "TNode.h"
+#include "Parser.h"
 
-int Parse () {
-
-	return 0;
+bool isValidVariableName(string variable)
+{
+    if (!((variable[0] >= 'a' && variable[0] <= 'z')
+          || (variable[0] >= 'A' && variable[0] <= 'Z')
+          || variable[0] == '_'))
+        return false;
+    for (int i = 1; i < variable.length(); i++) {
+        if (!((variable[i] >= 'a' && variable[i] <= 'z')
+              || (variable[i] >= 'A' && variable[i] <= 'Z')
+              || (variable[i] >= '0' && variable[i] <= '9')
+              || variable[i] == '_'))
+            return false;
+    }
+    return true;
 }
+
+bool isNumber(string str)
+{
+    std::string::const_iterator it = str.begin();
+    while (it != str.end() && std::isdigit(*it)) {
+        ++it;
+    }
+    return !str.empty() && it == str.end();
+}
+
+void parseAssign(vector<string> tokens) {
+    std::regex terms("(\\w+)");
+    std::smatch result;
+    std::vector<std::string> variableVector;
+    for (string token: tokens) {
+        std::regex_search(token, result, terms);
+            variableVector.push_back(result[0]);
+    }
+
+    for (string entity: variableVector)
+        if (isValidVariableName((entity))) {
+            std::cout<< entity << " variable" << endl;
+        } else if (isNumber(entity)) {
+            std::cout<< entity << " constant" << endl;
+        }
+}
+
+void parseStatement(vector<string> tokens) {
+    if (isValidVariableName(tokens.front())){
+        parseAssign(tokens);
+    } else if (tokens.front() == "if") {
+
+    } else if (tokens.front() == "while") {
+
+    } else if (tokens.front() == "print") {
+
+    } else if (tokens.front() == "read") {
+
+    } else {
+        throw std::invalid_argument("Invalid statement");
+    }
+}
+
--- Team25/Code25/src/spa/src/Parser.h
+++ Team25/Code25/src/spa/src/Parser.h
@@ -1,3 +1,15 @@
 #pragma once
+#include<stdio.h>
+#include <iostream>
+#include <string>
+#include <vector>
+
 using namespace std;
-int Parse();
+class Parser {
+    void parseStatement(vector<string> str);
+    void parseAssign(vector<string> str);
+    bool isValidVariableName(string str);
+    bool isNumber(string str);
+};;
+
+
