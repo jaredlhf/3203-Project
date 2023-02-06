84c27b921458d00a72d49269da9a3509e3b9f5b7
Jacob-109 <65383310+Jacob-109@users.noreply.github.com>
2023-02-06 19:37:38 +0800

    Add unit test for ExpressionParser

---
+++ Team25/Code25/src/unit_testing/src/TestExpressionParser.cpp
@@ -0,0 +1,36 @@
+
+#include "ExpressionParser.h"
+#include "ExpressionParser.cpp"
+#include "catch.hpp"
+#include <regex>
+#include <iostream>
+
+using namespace std;
+ExpressionParser e;
+TEST_CASE("ValidExpressionTest") {
+    REQUIRE(e.verifyExpr("x + y") == true);
+}
+
+TEST_CASE("ValidExpressionBracketTest") {
+    REQUIRE(e.verifyExpr("(x + y)") == true);
+}
+
+TEST_CASE("ValidExpressionMultiplyTest") {
+    REQUIRE(e.verifyExpr("x + (2 * y)") == true);
+}
+
+TEST_CASE("ValidComplicatedExpressionTest") {
+    REQUIRE(e.verifyExpr("x/3 + (2 * y) % (z - 3)") == true);
+}
+
+TEST_CASE("InvalidExpressionSuccessiveOperatorTest") {
+    REQUIRE(e.verifyExpr("x +  * y") == false);
+}
+
+TEST_CASE("InvalidBracketMatchTest") {
+    REQUIRE(e.verifyExpr("(x )+ y)") == false);
+}
+
+TEST_CASE("InvalidExpressionTest") {
+    REQUIRE(e.verifyExpr("(x") == false);
+}
