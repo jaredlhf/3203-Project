c82894a1578d598fd81a5024ac780a6888ea98a9
Nikki <thanwinnikki@gmail.com>
2023-02-06 16:47:57 +0800

    Add facade class methods for all storage classes

--- Team25/Code25/src/spa/src/PKB/PkbPopulator.cpp
+++ Team25/Code25/src/spa/src/PKB/PkbPopulator.cpp
@@ -6,19 +6,44 @@ using namespace std;
 
 #include "PkbPopulator.h"
 #include "VariableStore.h"
+#include "ConstantStore.h"
+#include "FollowsStore.h"
+#include "ProcedureStore.h"
+#include "StatementStore.h"
 
 /*
  * Constructor class for PkbPopulator
  * param: VarStorage* varStore
  */
-PkbPopulator::PkbPopulator(VariableStore* varStore) {
+PkbPopulator::PkbPopulator(VariableStore* varStore, ConstantStore* constStore, FollowsStore* followsStore, ProcedureStore* procedureStore, StatementStore* statementStore) {
 	this->varStorage = varStore;
+	this->constStorage = constStore;
+	this->followsStorage = followsStore;
+	this->procedureStorage = procedureStore;
+	this->statementStorage = statementStore;
 };
 
 
 void PkbPopulator::addVar(std::string varName) {
 	this->varStorage->addVar(varName);
 };
+
+void PkbPopulator::addConst(int constNum) {
+	this->constStorage->addConst(constNum);
+};
+
+void PkbPopulator::addFollows(int leftLineNum, int rightLineNum) {
+	this->followsStorage->addFollows(leftLineNum, rightLineNum);
+};
+
+void PkbPopulator::addProc(std::string procName) {
+	this->procedureStorage->addProc(procName);
+};
+
+void PkbPopulator::addStmt(std::string stmtType, int lineNum) {
+	this->statementStorage->addStmt(stmtType, lineNum);
+};
+
 
 
 
--- Team25/Code25/src/spa/src/PKB/PkbRetriever.cpp
+++ Team25/Code25/src/spa/src/PKB/PkbRetriever.cpp
@@ -6,19 +6,51 @@ using namespace std;
 
 #include "PkbRetriever.h"
 #include "VariableStore.h"
+#include "ConstantStore.h"
+#include "FollowsStore.h"
+#include "ProcedureStore.h"
+#include "StatementStore.h"
 
 /*
  * Constructor class for PkbRetriever
  * param: VarStorage* varStore
  */
-PkbRetriever::PkbRetriever(VariableStore* varStore) {
+PkbRetriever::PkbRetriever(VariableStore* varStore, ConstantStore* constStore, FollowsStore* followsStore, ProcedureStore* procedureStore, StatementStore* statementStore) {
 	this->varStorage = varStore;
+	this->constStorage = constStore;
+	this->followsStorage = followsStore;
+	this->procedureStorage = procedureStore;
+	this->statementStorage = statementStore;
 };
 
 std::unordered_set<std::string> PkbRetriever::getAllVar() {
 	return this->varStorage->getAllVar();
 }
 
-bool PkbRetriever::has(std::string varName) {
-	return this->varStorage->has(varName);
+std::unordered_set<int> PkbRetriever::getAllConst() {
+	return this->constStorage->getAllConst();
+}
+
+int PkbRetriever::getFollowee(int rightLineNum) {
+	return this->followsStorage->getFollowee(rightLineNum);
+}
+
+int PkbRetriever::getFollower(int leftLineNum) {
+	return this->followsStorage->getFollower(leftLineNum);
+}
+
+std::unordered_set<int> PkbRetriever::getAllFollowers() {
+	return this->followsStorage->getAllFollowers();
+}
+
+std::unordered_set<int> PkbRetriever::getAllFollowees() {
+	return this->followsStorage->getAllFollowees();
+}
+
+std::unordered_set<std::string> PkbRetriever::getAllProc() {
+	return this->procedureStorage->getAllProc();
+}
+
+std::unordered_set<int> PkbRetriever::getAllStmt(std::string stmtType) {
+	return this->statementStorage->getAllStmt(stmtType);
 }
