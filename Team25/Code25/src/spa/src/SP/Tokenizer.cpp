4e0ed0971cf1581d3300ab659aef57849d8cc61a
Jacob-109 <65383310+Jacob-109@users.noreply.github.com>
2023-02-12 21:31:08 +0800

    Implement tokenizer to differentiate for delimiters and identifiers

--- Team25/Code25/src/spa/src/SP/Tokenizer.cpp
+++ Team25/Code25/src/spa/src/SP/Tokenizer.cpp
@@ -29,18 +29,25 @@ vector<string> Tokenizer::tokenize(const string &str) {
 
     string next;
     vector<string> result;
-
+    const char delimiters_Args[] = {'\t', '\n', ' '};
+    std::vector<char> delimiters(delimiters_Args, std::end(delimiters_Args));
+    const char args[] = {'(', ')', ';'};
+    std::vector<char> identifiers(args, std::end(args));
     for (string::const_iterator it = str.begin(); it != str.end(); it++) {
-        if ((*it == ' ') || (*it == '\t'))  {
+        char curr = *it;
+        if (std::count(delimiters.begin(), delimiters.end(), curr))  {
             if (!next.empty()) {
                 result.push_back(next);
                 next.clear();
             }
-        } else if (*it == ';') {
+        } else if (std::count(identifiers.begin(), identifiers.end(), curr)) {
+            std::string id = std::string(1, curr);
             if (!next.empty()) {
                 result.push_back(next);
-                result.push_back(";");
+                result.push_back(id);
                 next.clear();
+            } else {
+                result.push_back(id);
             }
         } else {
             next += *it;
