80d386eaaf2e38cf309c12882e79e2576045357f
LAPTOP-PLO9FA4I\user <jsyl3214@gmail.com>
2023-02-17 00:48:15 +0800

    Add parent store and unit tests

--- Team25/Code25/src/spa/src/PKB/ParentStore.h
+++ Team25/Code25/src/spa/src/PKB/ParentStore.h
@@ -11,15 +11,14 @@ using namespace std;
 class ParentStore {
 private:
 	unordered_map<int, int> parentStore;
-	unordered_map<int, int> childStore;
+	unordered_map<int, std::unordered_set<int>> childrenStore;
 public:
 	ParentStore();
-	void addParent(int leftLineNum, int rightLineNum);
-	int getParent(int rightLineNum);
-	int getChild(int leftLineNum);
+	void addParent(int parent, int child);
+	int getParent(int child);
+	std::unordered_set<int> getChildren(int parent);
 	bool hasParent(int lineNum);
-	bool hasChild(int lineNum);
-	/*bool hasFollows(int leftLineNum, int rightLineNum);*/
+	bool hasChildren(int lineNum);
 	unordered_set<int> getAllParents();
 	unordered_set<int> getAllChildren();
 	void clear();
