bf0203317eb1e161ba6fcb2e245f3ee31749ad15
Jacob-109 <65383310+Jacob-109@users.noreply.github.com>
2023-02-19 23:40:38 +0800

    Add constants for tokenizers

--- Team25/Code25/src/spa/src/SP/Tokenizer.cpp
+++ Team25/Code25/src/spa/src/SP/Tokenizer.cpp
@@ -1,11 +1,9 @@
-#include<stdio.h>
-#include <iostream>
 #include <string>
 #include <vector>
+#include "Tokenizer.h"
+#include "SPConstants.h"
 
 using namespace std;
-
-#include "Tokenizer.h"
 
 void Tokenizer::setTokens( vector<string> tok ) {
     tokens = tok;
@@ -20,30 +18,41 @@ string Tokenizer::peek() {
 }
 
 string Tokenizer::getNextToken() {
+    if(tokens.empty()) {
+        throw std::invalid_argument("Invalid SIMPLE program");
+    }
     string next = this->tokens.front();
     tokens.erase(tokens.begin());
     return next;
 }
 
 vector<string> Tokenizer::tokenize(const string &str) {
-
+    //next token
     string next;
+    //result vector
     vector<string> result;
-    const char delimiters_Args[] = {'\t', '\n', ' '};
+    //token delimiters
+    const char delimiters_Args[] = {SPConstants::TAB,SPConstants::NEWLINE,SPConstants::SPACE};
     std::vector<char> delimiters(delimiters_Args, std::end(delimiters_Args));
-    const char identifiers_args[] = {'{', '}','(', ')', ';',  '+', '-','*', '/', '%', };
+    //single operator and identifier tokens
+    const char identifiers_args[] = {SPConstants::RIGHT_BRACE, SPConstants::LEFT_BRACE,SPConstants::RIGHT_BRACKET, SPConstants::LEFT_BRACKET, SPConstants::SEMICOLON,  SPConstants::PLUS, SPConstants::MINUS,SPConstants::TIMES, SPConstants::DIVIDE, SPConstants::MODULO, };
     std::vector<char> identifiers(identifiers_args, std::end(identifiers_args));
-    const char double_identifiers_args[] = {'=',  '>', '<', '!', '&', '|'};
+    //special tokens that could be the first token of a double char operator
+    const char double_identifiers_args[] = {SPConstants::EQUALS,  SPConstants::MORE_THAN, SPConstants::LESS_THAN, SPConstants::NOT, SPConstants::SINGLE_AND, SPConstants::SINGLE_OR};
     std::vector<char> double_identifiers(double_identifiers_args, std::end(double_identifiers_args));
-    const char second_operator_args[] = { '&', '|', '='};
+    //second token of a double char operator token
+    const char second_operator_args[] = { SPConstants::SINGLE_OR, SPConstants::SINGLE_AND, SPConstants::EQUALS};
     std::vector<char> second_operator(second_operator_args, std::end(second_operator_args));
+    // iterate through the characters of the input string
     for (string::const_iterator it = str.begin(); it != str.end(); it++) {
         char curr = *it;
+        //check for delimiters
         if (std::count(delimiters.begin(), delimiters.end(), curr))  {
             if (!next.empty()) {
                 result.push_back(next);
                 next.clear();
             }
+        //check for identifiers
         } else if (std::count(identifiers.begin(), identifiers.end(), curr)) {
             std::string id = std::string(1, curr);
             if (!next.empty()) {
@@ -53,6 +62,7 @@ vector<string> Tokenizer::tokenize(const string &str) {
             } else {
                 result.push_back(id);
             }
+        //check for possible double char operators
         } else if (std::count(double_identifiers.begin(), double_identifiers.end(), curr)) {
             std::string id = std::string(1, curr);
             if (!next.empty() && (std::count(second_operator.begin(), second_operator.end(), *(it + 1)))) {
@@ -76,7 +86,7 @@ vector<string> Tokenizer::tokenize(const string &str) {
     }
     if (!next.empty())
         result.push_back(next);
-
+    //set tokens of tokenizer
     setTokens(result);
     return result;
 }
