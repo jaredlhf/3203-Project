fba5137cb9b976cbf5f7905250776ed45883cc5d
ngsengleng <ngsengleng@gmail.com>
2023-02-20 00:47:08 +0800

    add more unit tests to queryParser

--- Team25/Code25/src/spa/src/QPS/QueryParser.h
+++ Team25/Code25/src/spa/src/QPS/QueryParser.h
@@ -42,45 +42,16 @@ class ValidatePatternResponse {
         }
 };
 
-class ValidateSuchThatResponse {
-    private:
-        std::string keyword = "";
-        std::shared_ptr<Entity> firstRef;
-        std::shared_ptr<Entity> secondRef;
-    public: 
-        void setKeyword(std::string s) {
-            this->keyword = s;
-        }
-        void setFirstRef(std::shared_ptr<Entity> e) {
-            this->firstRef = e;
-        }
-        void setSecondRef(std::shared_ptr<Entity> e) {
-            this->secondRef = e;
-        }
-        std::string getKeyword() {
-            return this->keyword;
-        }
-        std::shared_ptr<Entity> getFirstRef() {
-            return this->firstRef;
-        }
-        std::shared_ptr<Entity> getSecondRef() {
-            return this->secondRef;
-        }
-        bool isIncomplete() {
-            return this->keyword == "" || this->secondRef == nullptr || this->firstRef == nullptr; 
-        }
-};
-
 class QueryParser {
     private:
-        bool isValidDeclaration(std::vector<std::string> s,
+        static bool isValidDeclaration(std::vector<std::string> s,
             std::unordered_set<std::string>& declared_synonyms, 
             std::unordered_set<std::string>& assignment_synonyms);
-        std::vector<std::shared_ptr<Synonym>> processDeclaration(std::vector<std::string> declaration, std::unordered_set<std::string> declared_synonyms);
-        ValidatePatternResponse validatePatternClause(std::vector<std::string> s, std::unordered_set<std::string> assignment_synonyms, std::vector<std::shared_ptr<Synonym>> declarations);
-        std::vector<std::string> validateSuchThatClause(std::vector<std::string> s);
-        ParserResponse generateSyntaxErrorResponse();
-        ParserResponse generateSemanticErrorResponse();
+        static std::vector<std::shared_ptr<Synonym>> processDeclaration(std::vector<std::string> declaration, std::unordered_set<std::string> declared_synonyms);
+        static ValidatePatternResponse validatePatternClause(std::vector<std::string> s, std::unordered_set<std::string> assignment_synonyms, std::vector<std::shared_ptr<Synonym>> declarations);
+        static std::vector<std::string> validateSuchThatClause(std::vector<std::string> s);
+        static ParserResponse generateSyntaxErrorResponse();
+        static ParserResponse generateSemanticErrorResponse();
 
     public:
         ParserResponse parseQueryTokens(std::vector<std::string> tokens);
