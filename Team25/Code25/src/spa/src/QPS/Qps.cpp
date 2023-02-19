77cf520ef579cccd0d68c200bcf45e0a3a615e12
jaredlhf <jlhfqaz@gmail.com>
2023-02-19 15:58:46 +0800

    Remove print lines

--- Team25/Code25/src/spa/src/QPS/Qps.cpp
+++ Team25/Code25/src/spa/src/QPS/Qps.cpp
@@ -5,13 +5,8 @@ Qps::Qps(std::shared_ptr<PkbRetriever> retriever) {
 }
 
 void Qps::query(const std::string& queryStr, std::list<std::string>& uiList) {
-    std::cout<< "here1" <<std::endl;
 	std::vector<std::string> tokens = tokenizer.tokenize(queryStr);
-    std::cout<< "here2" <<std::endl;
 	ParserResponse response = parser.parseQueryTokens(tokens);
-    std::cout<< "here3" <<std::endl;
 	std::list<std::string> result = evaluator.evaluate(response, pkbRetriever);
-    std::cout<< "here4" <<std::endl;
 	uiList.insert(uiList.end(), result.begin(), result.end());
-    std::cout<< "here5" <<std::endl;
 }
