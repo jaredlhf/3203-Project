8de40bf3d90d3a61aefbd0983b5fba3d8d59d184
Nikki <thanwinnikki@gmail.com>
2023-03-05 00:54:35 +0800

    Add algo for postfix notation

--- Team25/Code25/src/spa/src/QPS/utils/StringUtils.cpp
+++ Team25/Code25/src/spa/src/QPS/utils/StringUtils.cpp
@@ -67,7 +67,66 @@ std::vector<std::string> StringUtils:: splitString(const std::string& s) {
 */
 bool StringUtils::tokenInOp(const std::string& opStr, const std::string& token) {
 	std::vector<std::string> opTokens = StringUtils::splitString(StringUtils::spaceifyString(opStr));
-
 	return std::find(opTokens.begin(), opTokens.end(), token) != opTokens.end();
+}
+
+/*
+	Creates postfix notation expression string when given an prefix notation expression string
+*/
+std::string StringUtils::createPostFixNotation(const std::string& prefix) {
+	std::string result;
+	std::vector<std::string> prefixVector;
+	std::stack<char> opStack;
+
+	for (const char c : prefix) {
+		// if its an expression 
+		if (std::isalnum(c)) {
+			result.push_back(c);
+		}
+		// if its a spacing
+		else if (c == ' ' || c == '\t') {
+			// ignore white space and tab spacing
+		}
+		// if its an operator or brackets
+		else {
+			// add all operators on opStack to string
+			while (!opStack.empty() && opStack.top() != ')' && opStack.top() != '(' &&
+				StringUtils::opPrecedence(c) < StringUtils::opPrecedence(opStack.top())) {
+				result.push_back(opStack.top());
+				opStack.pop();
+			}
+			opStack.push(c);
+		}
+	}
+	
+	while (!opStack.empty()) {
+		result.push_back(opStack.top());
+		opStack.pop();
+	}
+	std::reverse(result.begin(), result.end());	
+	return result;
+}
+
+/*
+	Returns an integer to represent the precedence of operations, higher integer has higher precedence
+*/
+
+int StringUtils::opPrecedence(char op) {
+	if (op == '+' || op == '-') {
+		return 1;
+	}
+	else if (op == '*' || op == '/') {
+		return 2;
+	}
+	else {
+		return 0;
+	}
+}
+
+bool StringUtils::postFixInFullpostFix(const std::string& postFix, const std::string& fullPostFix) {
+	if (fullPostFix.find(postFix) != std::string::npos) {
+		return true;
+	}
+	return false;
 }
 
