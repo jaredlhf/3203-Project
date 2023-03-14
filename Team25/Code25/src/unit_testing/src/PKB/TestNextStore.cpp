240282780698ea439df7def8bdd2861982ac2fa5
LAPTOP-PLO9FA4I\user <jsyl3214@gmail.com>
2023-03-14 17:26:24 +0800

    Add test case for next*

--- Team25/Code25/src/unit_testing/src/PKB/TestNextStore.cpp
+++ Team25/Code25/src/unit_testing/src/PKB/TestNextStore.cpp
@@ -51,3 +51,38 @@ SCENARIO("Populating next store") {
 		}
 	}
 }
+
+SCENARIO("Retrieving Next* relation") {
+	GIVEN("Instance of Next store with values") {
+		NextStore nextStore;
+		nextStore.addNext(1, 2);
+		nextStore.addNext(1, 3);
+		nextStore.addNext(3, 4);
+		nextStore.addNext(3, 5);
+		nextStore.addNext(5, 6);
+		nextStore.addNext(5, 7);
+		nextStore.addNext(8, 9);
+		nextStore.addNext(8, 10);
+
+		THEN("The next relations should be mapped correctly") {
+			REQUIRE(nextStore.getAllLeft().size() == 4);
+			REQUIRE(nextStore.getAllRight().size() == 8);
+			REQUIRE(nextStore.getRightStmt(3).size() == 2);
+			REQUIRE(nextStore.getRightStmt(8).size() == 2);
+			REQUIRE(nextStore.getLeftStmt(3).size() == 1);
+			REQUIRE(nextStore.getLeftStmt(8).size() == 0);
+		}
+
+		THEN("The next* relations should be mapped correctly") {
+			REQUIRE(nextStore.getRightStmtStar(1) == std::unordered_set<int>({2, 3, 4, 5, 6, 7}));
+			REQUIRE(nextStore.getRightStmtStar(8) == std::unordered_set<int>({ 9, 10 }));
+			REQUIRE(nextStore.getRightStmtStar(10) == std::unordered_set<int>({ }));
+			REQUIRE(nextStore.getLeftStmtStar(1) == std::unordered_set<int>({ }));
+			REQUIRE(nextStore.getLeftStmtStar(7) == std::unordered_set<int>({ 1, 3, 5 }));
+			REQUIRE(nextStore.getLeftStmtStar(10) == std::unordered_set<int>({ 8 }));
+
+		}
+
+
+	}
+}
